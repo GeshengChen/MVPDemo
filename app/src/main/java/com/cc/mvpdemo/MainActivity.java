666d40f5551be8e6c816036f4a9c22e307c1bd13
gesheng <3510048110@qq.com>
2016-10-30 16:49:33 +0800

    init commit

--- app/src/main/java/com/cc/mvpdemo/MainActivity.java
+++ app/src/main/java/com/cc/mvpdemo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.cc.mvpdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.iv)
+    TextView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        iv.setText("Hello");
     }
 }
--- zz_note/config_gradle.md
+++ zz_note/config_gradle.md
@@ -9,3 +9,4 @@ apply plugin: 'android-apt'
 apt 'com.jakewharton:butterknife-compiler:8.4.0'
 compile 'com.jakewharton:butterknife:8.4.0'
 ```
+=========================华丽的分割线==========================================
